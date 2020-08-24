96ce3b012ad3b8dc7f5a1e711bc6004fca97e7ab
feychou <federica.recanatini@gmail.com>
2020-08-24 15:21:50 +0200

    Create pokemon subset

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ function App() {
   const [pokemon, setPokemon] = useState([]);
   const [offset, setOffset] = useState(0);
   const [types, setTypes] = useState([]);
+  const [activeType, setActiveType] = useState('');
 
   const getAllTypes = () => {
     axios
       .get("https://pokeapi.co/api/v2/type")
       .then((response) => response.data.results)
       .then((resTypes) => {
-        const typesNoShadow = resTypes.filter(({ name }) => name !== "shadow")
-        const typePromises = typesNoShadow.map(({ url }) =>  axios.get(url).then((type) => type.data ));
+        const realTypes = resTypes.filter(({ name }) => name !== "shadow" && name !== "unknown");
+        const typePromises = realTypes.map(({ url }) =>  axios.get(url).then((type) => type.data ));
         Promise.all(typePromises).then(results => setTypes(results))
       })
       .catch((error) => {
@@ -26,7 +27,7 @@ function App() {
 
   const loadPokemon = () => {
     axios
-      .get(`https://pokeapi.co/api/v2/pokemon?limit=9&offset=${offset}`)
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=721`)
       .then((response) => setPokemon(response.data.results))
       .catch((error) => {
         console.log("Request failed");
@@ -36,12 +37,29 @@ function App() {
   useEffect(getAllTypes, []);
   useEffect(loadPokemon, [offset]);
 
+  const getOffsetPokemon = (activePokemon) => activePokemon.slice(offset, offset + 9);
+  
+  const getPokemonToDisplay = () => {
+    if (!activeType) return getOffsetPokemon(pokemon);
+
+    const activePokemon =  types.find(({ name }) => name === activeType).pokemon;
+    const activePokemonNames = activePokemon.map(({pokemon}) => pokemon.name);
+    const activePokemonSubset = pokemon.filter(({name}) => activePokemonNames.includes(name))
+    const offsetPokemon = getOffsetPokemon(activePokemonSubset);
+    return offsetPokemon;
+  }
+
   const onClickPrev = () => {
     setOffset(offset - 9);
   };
 
   const onClickNext = () => {
     setOffset(offset + 9);
+  };
+
+  const onFilterType = (type) => {
+    setOffset(0);
+    setActiveType(type)
   };
 
   return (
@@ -51,13 +69,14 @@ function App() {
           <span
             key={name}
             className={classnames("TypeContainer", `TypeContainer--${name}`)}
+            onClick={() => onFilterType(name)}
           >
             <span className="TypeName">{name}</span>
           </span>
         ))}
       </div>
       <div className="PokemonWrapper">
-        {pokemon.map(({ name }, index) => (
+        {getPokemonToDisplay().map(({ name }, index) => (
           <div className="Pokemon" key={index}>
             <img
               alt={name}
